f6a70146caae12e5bea6f8c1edd397b8a5b82131
Matthew J Morrison <mattj.morrison@gmail.com>
2010-07-31 08:55:00 -0500

    added data to template

--- microblog/views.py
+++ microblog/views.py
@@ -1,4 +1,5 @@
 from django import shortcuts
+from cijug.microblog import models
 
 def index(request):
-    return shortcuts.render_to_response('index.html')
+    return shortcuts.render_to_response('index.html', {'blogs':models.Blog.objects.all()})
