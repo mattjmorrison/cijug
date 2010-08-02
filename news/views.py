cda409bbb3f18400518ec39f0f8d61a2353b6149
Matthew J Morrison <mattj.morrison@gmail.com>
2010-08-01 21:27:44 -0500

    added use of Java from Jython

--- news/views.py
+++ news/views.py
@@ -1,4 +1,5 @@
 from django import shortcuts
+from java.util import Date
 
 def index(request):
-    return shortcuts.render_to_response('news/templates/index.html')
+    return shortcuts.render_to_response('news/templates/index.html', {'date': Date()})
