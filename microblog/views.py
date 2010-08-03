75ec9a3be3931583e8748a3656949a00d2019e31
Matthew J Morrison <mattj.morrison@gmail.com>
2010-08-02 19:24:17 -0500

    optimized sql statements

--- microblog/views.py
+++ microblog/views.py
@@ -3,7 +3,7 @@ from cijug.microblog import models, forms
 from django.core.urlresolvers import reverse
 
 def index(request):
-    return shortcuts.render_to_response('microblog/templates/index.html', {'blogs':models.Blog.objects.all()})
+    return shortcuts.render_to_response('microblog/templates/index.html', {'blogs':models.Blog.objects.select_related().all()})
 
 def add(request):
     return shortcuts.render_to_response('microblog/templates/add.html', {'blog_form':forms.BlogForm()})
@@ -17,7 +17,7 @@ def save_add(request):
         return shortcuts.render_to_response('microblog/templates/add.html', {'blog_form':form})
 
 def edit(request, id):
-    instance = models.Blog.objects.get(id=id)
+    instance = models.Blog.objects.select_related().get(id=id)
     return shortcuts.render_to_response('microblog/templates/edit.html', {'blog_form':forms.BlogForm(instance=instance), 'blog':instance})
 
 def save_edit(request, id):
